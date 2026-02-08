6dd0b55f6460449f91946d94a7688fd496210bb0
ujin22 <p.b.m.aubert@gmail.com>
2026-02-08 17:58:03 +0100

    Test server connection

--- src/app.ts
+++ src/app.ts
@@ -9,5 +9,7 @@ const port = process.env["PORT"] || 3000;
 const app = express();
 
 app.use(bodyParser.json());
-
-app.listen(port, () => console.log(`Server listening on port ${port}`));
+app.get("/", (_req, res)=> {
+    res.status(200).json("Hello world!");
+})
+app.listen(port, () => console.log(`Server listening on http://localhost:${port}`));
