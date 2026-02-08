59420ced7c7fb2aa7daceec4ed06fd8e277f322b
ujin22 <p.b.m.aubert@gmail.com>
2026-02-08 17:47:42 +0100

    Create minimal server

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,13 @@
-console.log("HELLO WORLD!");
+import express from "express";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const port = process.env["PORT"] || 3000;
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.listen(port, () => console.log(`Server listening on port ${port}`));
